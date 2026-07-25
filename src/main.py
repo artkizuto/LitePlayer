ffc255553fc04df07aaf7da2e37ad4f709cd7ca8
artkizuto <artkizuto.contact@gmail.com>
2026-07-25 21:38:25 +0700

    feat(ui): add application icon

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
 
+from PySide6.QtGui import QIcon  # 1. Import thêm QIcon
 from PySide6.QtWidgets import QApplication
 
 from window import MainWindow
@@ -9,6 +10,8 @@ def main():
     app = QApplication(sys.argv)
 
     window = MainWindow()
+    # 2. Đặt setWindowIcon ở đây, sau khi window đã được tạo
+    window.setWindowIcon(QIcon("assets/icon.ico"))
     window.show()
 
     sys.exit(app.exec())
